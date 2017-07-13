97ff56fe39a829b42c8794af42a056d349efe4f3
Eric Leschinski <eric.leschinski@hotmail.com>
2017-07-13 00:20:26 -0400

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,6 @@
 
 echo "k"
 
-scp -i /home/el/.ssh/id_rsa profile.html machines@machinesentience.com:public_html/profile.html
+scp -i /var/lib/jenkins/id_rsa profile.html machines@machinesentience.com:public_html/profile.html
 
 cd /var/lib/jenkins/workspace/stackoverflowprofile; sudo /usr/bin/html2ps profile.html > ~/profile.ps; sudo cp ~/profile.ps /var/lib/jenkins/workspace/stackoverflowprofile/profile.ps
